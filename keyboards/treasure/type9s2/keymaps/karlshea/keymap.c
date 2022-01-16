314c5a04bfea87a027a11ec772501941e44a7934
Karl Shea <karl@weilstreet.com>
2022-01-16 02:08:18 -0600

    Cut/Copy/Paste macros / Disable VIA

--- keyboards/treasure/type9s2/keymaps/karlshea/keymap.c
+++ keyboards/treasure/type9s2/keymaps/karlshea/keymap.c
@@ -1,10 +1,18 @@
 #include QMK_KEYBOARD_H
 
+// Define three custom keycodes
+enum custom_keycodes {
+    MY_CUT = SAFE_RANGE,
+    MY_CPY,
+    MY_PST,
+};
+
+// Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_3x3(
         MO(1),   KC_MPLY, BL_STEP,
         KC_MPRV, KC_MUTE, KC_MNXT,
-        KC_KP_1, KC_KP_2, KC_KP_3
+        MY_CUT,  MY_CPY,  MY_PST
     ),
 
     [1] = LAYOUT_ortho_3x3(
@@ -25,3 +33,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET,   _______, _______
     )
 };
+
+
+// Custom keycode handler
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+
+        // Cut
+        case MY_CUT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCMD("x")); // Left cmd + x
+            }
+            return false;
+
+        // Copy
+        case MY_CPY:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCMD("c")); // Left cmd + c
+            }
+            return false;
+
+        // Paste
+        case MY_PST:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCMD("v")); // Left cmd + v
+            }
+            return false;
+
+    }
+    return true;
+}
