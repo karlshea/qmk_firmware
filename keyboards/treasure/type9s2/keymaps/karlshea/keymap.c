1ae0584b3b02452461665b77ec2080a8dccd4cf5
Karl Shea <karl@weilstreet.com>
2022-12-26 21:25:00 -0600

    Fix keymap

--- keyboards/treasure/type9s2/keymaps/karlshea/keymap.c
+++ keyboards/treasure/type9s2/keymaps/karlshea/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT_ortho_3x3(
         _______, _______, _______,
         _______, _______, _______,
-        RESET,   _______, _______
+        QK_BOOT, _______, _______
     )
 };
 
